a75b9dd9737041ff31b2f4624aaa7d06669c2af8
rezahusain <rezahusain99@gmail.com>
2024-06-10 16:43:09 -0500

    Updated director page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,4 +3,10 @@ class DirectorsController < ApplicationController
     render({:template => "director_templates/list"})
   end
 
+  def show
+    the_id = params.fetch("the_id")
+    @the_director = Director.where({:id => the_id}).at(0)
+    render({:template=>"director_templates/details"})
+  end
+
 end
